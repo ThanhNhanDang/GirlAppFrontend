65f9262f02227a7517a85c422ccc501264ab76f9
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2021-09-16 15:15:56 +0700

    Updates

---
+++ precache-manifest.989fe0c66eaa5e0874f3b735378794aa.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d15676b865980bf4aae4",
+    "url": "/GirlAppFrontend/static/js/main.d15676b8.chunk.js"
+  },
+  {
+    "revision": "c2e537f3c2cb020f1898",
+    "url": "/GirlAppFrontend/static/js/1.c2e537f3.chunk.js"
+  },
+  {
+    "revision": "ef5b28a992c95ac9fd7b",
+    "url": "/GirlAppFrontend/static/js/runtime~main.ef5b28a9.js"
+  },
+  {
+    "revision": "62f9b2351312ffc7c54e9991a0398f81",
+    "url": "/GirlAppFrontend/index.html"
+  }
+];
